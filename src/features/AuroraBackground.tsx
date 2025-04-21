04180d18b1081ca5a3606f98347c292e49f5dc4d
Carlos David <apolayamendozaCarlos.com>
2025-04-20 21:33:41 -0500

    Se añaden los botones de contacto y sus estilos al Home

--- src/features/AuroraBackground.tsx
+++ src/features/AuroraBackground.tsx
@@ -5,6 +5,11 @@ import React from "react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import BackEnd from "@/components/skills/Back-End";
 import FrontEnd from "@/components/skills/Front-End";
+import Link from "next/link";
+import Gmail from "@/components/svg/Gmail";
+import LinkedIn from "@/components/svg/LinkedIn";
+import GitHub from "@/components/svg/GitHub";
+import "@/css/Home.css";
 
 export function AuroraBackgroundDemo() {
   return (
@@ -18,24 +23,49 @@ export function AuroraBackgroundDemo() {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="relative flex flex-col gap-4 items-center justify-center px-4"
+          className="relative flex flex-col gap-6 items-center justify-center px-4 py-10"
         >
-          <div className="text-3xl md:text-5xl 2xl:text-7xl font-bold dark:text-white text-center">
-            Carlos David A. Mendoza
+          <h1 className="title">Carlos David A. Mendoza</h1>
+
+          <div className="text-center space-y-2">
+            <h2 className="subtitle">Full Stack Developer &lt;/&gt;</h2>
+            <p className="description">
+              Microservicios con Java y Spring-Boot e Interfaces de usuario con
+              React.js
+            </p>
           </div>
-          <div className="font-extralight text-base md:text-2xl 2xl:text-4xl dark:text-neutral-200 py-4">
-            Portafolio Moderno de Carlos David A. Mendoza
+
+          <div className="contact-container">
+            <a
+              href="mailto:apolayamendozacarlos@gmail.com"
+              target="_blank"
+              className="link-gmail"
+            >
+              <Gmail className="inline-block mr-2" />
+              Email
+            </a>
+            <Link
+              href="https://www.linkedin.com/in/carlosdavidfront-end/"
+              target="_blank"
+              className="LinkedIn-GitHub"
+            >
+              <LinkedIn className="inline-block mr-2" />
+              LinkedIn
+            </Link>
+            <Link
+              href="https://github.com/CarlosDavid-AM"
+              target="_blank"
+              className="LinkedIn-GitHub"
+            >
+              <GitHub className="inline-block mr-2 bg-black dark:bg-none rounded-full dark:rounded-none" />
+              GitHub
+            </Link>
           </div>
-          <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-            Debug now
-          </button>
         </motion.div>
 
         {/* Skills */}
-        <div
-          id="skills"
-          className="flex flex-col lg:flex-row justify-center items-center gap-10 2xl:gap-x-36 w-full max-w-6xl px-4 mx-auto mt-8"
-        >
+        <h2 className="title-skill">Skills</h2>
+        <div id="skills" className="front-back">
           <FrontEnd />
           <BackEnd />
         </div>
